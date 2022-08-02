52b14699226dfca1240c85d803b8228f985935f6
Hermes Emmanuel <hermesemmanueldev@gmail.com>
2022-08-02 00:44:43 -0300

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     console.log(err);
   } else {
