37b7e2fe2d6ee90cc046630de393d39b5768a876
Hermes Emmanuel <hermesemmanueldev@gmail.com>
2022-08-02 00:12:58 -0300

    add server file

---
+++ server.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server is running');
+  }
+});
